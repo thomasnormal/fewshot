bc5126b50508a300440ac6deb4432395b7dfac28
Thomas D Ahle <thomas@ahle.dk>
2024-08-05 21:26:41 -0700

    More examples

--- examples/hotpot.py
+++ examples/hotpot.py
@@ -2,8 +2,7 @@ from datasets import load_dataset
 from pydantic import Field, BaseModel
 import instructor, openai, dotenv
 import asyncio
-from tqdm import tqdm
-from itertools import islice
+from tqdm.asyncio import tqdm
 
 from fewshot import Predictor, Example
 from optimizers import OptunaFewShot
@@ -11,9 +10,7 @@ from optimizers import OptunaFewShot
 
 class Question(BaseModel):
     """Answer questions with short factoid answers."""
-
     question: str
-
 
 class Answer(BaseModel):
     reasoning: str = Field(description="reasoning for the answer")
@@ -22,28 +19,17 @@ class Answer(BaseModel):
 
 async def main():
     dataset = load_dataset("hotpot_qa", "fullwiki")
-    trainset = [
-        (Question(question=x["question"]), x["answer"])
-        for x in islice(dataset["train"], 100)
-    ]
+    trainset = [(Question(question=x["question"]), x["answer"]) for x in dataset["train"]]
 
     dotenv.load_dotenv()
     client = instructor.from_openai(openai.AsyncOpenAI())
-    pred = Predictor(
-        client, "gpt-4o-mini", output_type=Answer, optimizer=OptunaFewShot(3)
-    )
+    pred = Predictor(client, "gpt-4o-mini", output_type=Answer, optimizer=OptunaFewShot(3))
 
-    correctness = []
-    with tqdm(total=len(trainset)) as pbar:
-        async for (input, expected), answer in pred.as_completed(
-            trainset, max_concurrent=10
-        ):
+    with tqdm(pred.as_completed(trainset), total=len(trainset)) as pbar:
+        async for (input, expected), answer in pbar:
             score = int(answer.answer == expected)
             pred.backwards(input, answer, score)
-
-            correctness.append(score)
-            pbar.set_postfix(correctness=sum(correctness) / len(correctness))
-            pbar.update(1)
+            pbar.set_postfix(correctness=sum(ex.score for ex in pred.log) / len(pred.log))
 
     pred.inspect_history()
 
